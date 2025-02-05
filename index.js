ef0bf70380edb970f1e2ed0dc1f3b9041a7c0a66
aman78954 <125014489+aman78954@users.noreply.github.com>
2025-02-05 14:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-hunt-backend-seven.vercel.app/"],
+  origin: ["https://job-hunt-backend-seven.vercel.app"],
   credentials: true,
 };
 
